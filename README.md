a75326dd5bb7fb75dc8eaa102bd31da9c3db3358
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2020-06-27 16:54:52 -0300

    Finalação do Projeto

---
+++ README.md
@@ -0,0 +1,12 @@
+# cursomc
+Curso da Udemecy : Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB
+Parte 2: Capítulo: Operações de CRUD e Casos de Uso
+Emenda: 
+Implementar requisições POST, GET, PUT e DELETE para inserir, obter, atualizar e deletar
+entidades, respectivamente, seguindo boas práticas REST e de desenvolvimento em camadas.
+- Trabalhar com DTO (Data Transfer Object)
+- Trabalhar com paginação de dados
+- Trabalhar com validação de dados com Bean Validation (javax.validation)
+- Criar validações customizadas
+- Fazer tratamento adequado de exceções (incluindo integridade referencial e validação)
+- Efetuar consultas personalizadas ao banco de dados
---
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -0,0 +1,98 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.domain.ItemPedido;
+import com.nelioalves.cursomc.domain.PagamentoComBoleto;
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.repositories.ItemPedidoRepository;
+import com.nelioalves.cursomc.repositories.PagamentoRepository;
+import com.nelioalves.cursomc.repositories.PedidoRepository;
+import com.nelioalves.cursomc.security.UserSS;
+import com.nelioalves.cursomc.services.exceptions.AuthorizationException;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EmailService emailService;
+
+	public Pedido find(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pgto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pgto, obj.getInstante());
+		}
+
+		obj = repo.save(obj);
+
+		pagamentoRepository.save(obj.getPagamento());
+
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPedido(obj);
+		}
+
+		itemPedidoRepository.save(obj.getItens());
+		emailService.sendOrderConfirmationHtmlEmail(obj);
+		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+
+		if (user == null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
+}
